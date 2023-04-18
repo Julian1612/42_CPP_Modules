c847b2c118059a68171cf6aa7cfb96facae990cd
Julian <92728359+Julian1612@users.noreply.github.com>
2023-04-18 20:25:37 +0200

    finished first exercise of cpp00 ex00:

---
+++ cpp_00/ex00/megaphone.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jschneid <jschneid@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/18 19:34:26 by jschneid          #+#    #+#             */
+/*   Updated: 2023/04/18 20:24:57 by jschneid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+void	to_uppercase(const std::string str)
+{
+	int i = 0;
+
+	while(str[i])
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			std::cout << static_cast<char>(str[i] - 32);
+		else
+			std::cout << str[i];
+		i++;
+	}
+}
+
+int	main(int argc, const char *argv[])
+{
+	int	i = 1;
+
+	while(i < argc)
+	{
+		to_uppercase(argv[i]);
+		i++;
+	}
+	std::cout << std::endl;
+	return (0);
+}
