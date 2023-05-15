33963a8bb0a1b5e48f2f20e48a075c6e509f9f28
Julian <92728359+Julian1612@users.noreply.github.com>
2023-05-15 14:26:37 +0200

    finished cpp01 ex02

---
+++ cpp_01/ex02/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jschneid <jschneid@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/15 14:08:31 by jschneid          #+#    #+#             */
+/*   Updated: 2023/05/15 14:26:16 by jschneid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int main(void)
+{
+	std::string	string = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &string;
+	std::string &stringREF = string;
+
+	std::cout << "Memory address of the string variable: " << &string << std::endl;
+	std::cout << "Memory address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "Memory address held by stringREF: " << &stringREF << std::endl;
+	std::cout << "Value of the string variable: " << string << std::endl;
+	std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value pointed to by stringREF: " << stringREF << std::endl;
+	return (0);
+}
