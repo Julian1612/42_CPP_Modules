6e02f4727b650307a46ce557fd90e282d3116ec9
Julian <92728359+Julian1612@users.noreply.github.com>
2023-05-15 12:54:53 +0200

    safe

--- cpp_01/ex01/main.cpp
+++ cpp_01/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jschneid <jschneid@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 11:36:52 by jschneid          #+#    #+#             */
-/*   Updated: 2023/05/15 12:30:04 by jschneid         ###   ########.fr       */
+/*   Updated: 2023/05/15 12:53:36 by jschneid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int main(void)
 {
 	Zombie* Horde;
 	std::string ZombiesName = "Morty";
-	int number_of_zombies = 4;
+	int number_of_zombies = 50;
 
 	Horde = zombieHorde(number_of_zombies, ZombiesName);
 	for (int i = 0; i < number_of_zombies; i++)
