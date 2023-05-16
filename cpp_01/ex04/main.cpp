ed45874260b5122c8294d6c3e7961bcd485474c0
Julian <92728359+Julian1612@users.noreply.github.com>
2023-05-16 11:19:49 +0200

    finished cpp01 ex04

---
+++ cpp_01/ex04/main.cpp
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jschneid <jschneid@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/15 19:00:57 by jschneid          #+#    #+#             */
+/*   Updated: 2023/05/16 11:19:26 by jschneid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+bool replacer(const std::string& file_name, const std::string& arg1, const std::string& arg2)
+{
+	std::string line;
+	std::ifstream file(file_name.c_str());
+	if (!file.is_open())
+	{
+		std::cout << "Error! Failed to open the file" << std::endl;
+		return (false);
+	}
+	std::ofstream output_file("output.txt");
+	while (std::getline(file, line))
+	{
+		if (line.find(arg1) != std::string::npos)
+		{
+			size_t pos = 0;
+			while ((pos = line.find(arg1, pos)) != std::string::npos)
+			{
+				line.erase(pos, arg1.length());
+				line.insert(pos, arg2);
+				// pos += arg2.length();
+			}
+		}
+		output_file << line << std::endl;
+	}
+	file.close();
+	output_file.close();
+	return (true);
+}
+
+int main(int argc, char** argv) {
+	if (argc != 4) {
+		std::cout << "Error! The number of arguments is wrong" << std::endl;
+		return 1;
+	}
+	if (replacer(argv[1], argv[2], argv[3]))
+		return 1;
+	return 0;
+}
